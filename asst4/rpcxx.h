0590ddc676bd1e856e85d304719bf7c0778517e3
Wasif <wasif.butt@mail.utoronto.ca>
2020-11-26 00:34:08 -0500

    holy shit did i just fix our git, task 1 passing

--- asst4/rpcxx.h
+++ asst4/rpcxx.h
@@ -16,15 +16,15 @@ namespace rpc {
 // types.
 template <typename T> struct Protocol;
 
-template <> struct Protocol<int> {
-  static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const int &x) {
+template <typename T> struct four_byte {
+  static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const T &x) {
     if (*out_len < 4) return false;
     uint32_t bx = htonl((uint32_t) x);
     memcpy(out_bytes, &bx, 4);
     *out_len = 4;
     return true;
   }
-  static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, int &x) {
+  static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, T &x) {
     if (*in_len < 4) return false;
     uint32_t bx = 0;
     memcpy(&bx, in_bytes, 4);
@@ -33,6 +33,68 @@ template <> struct Protocol<int> {
     return true;
   }
 };
+
+template <> struct Protocol<std::string> {
+  static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const std::string &x) {
+    int len = x.length();
+    uint32_t bit_len = 8 + (~(0x03) & (len - 1)); 
+    if (*out_len < bit_len) return false;
+    int bx = htobe32(len); 
+    memcpy(out_bytes, &bx, 4);
+    memcpy(out_bytes + 4, x.data(), len);
+    *out_len = bit_len;
+    return true;
+  }
+  static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, std::string &x) {
+    if (*in_len < 4) return false;
+    int bx = 0;
+    memcpy(&bx, in_bytes, 4);
+    int content = be32toh(bx); 
+    uint32_t bit_len = 8 + ((content - 1) & (~0x03)); 
+    if (*in_len < bit_len) return false;
+    if (content == 0) x = std::string("");
+    else {
+      x = std::string(content, '\0'); 
+      memcpy(&x.front(), in_bytes + 4, content);
+    }
+    *in_len = bit_len; 
+    return true; 
+  }
+};
+
+template <typename T> struct eight_byte {
+  static bool Encode(uint8_t *out_bytes, uint32_t *out_len, const T &x) {
+    if (*out_len < 8) return false;
+    uint64_t bx = htobe64((uint64_t) x);
+    memcpy(out_bytes, &bx, 8);
+    *out_len = 8;
+    return true;
+  }
+  static bool Decode(uint8_t *in_bytes, uint32_t *in_len, bool *ok, T &x) {
+    if (*in_len < 8) return false;
+    uint64_t bx = 0;
+    memcpy(&bx, in_bytes, 8);
+    x = be64toh(bx);
+    *in_len = 8;
+    return true;
+  }
+};
+
+template <> struct Protocol<int> : public four_byte<int>{};
+template <> struct Protocol<bool> : public four_byte<bool>{};
+template <> struct Protocol<char> : public four_byte<char>{};
+template <> struct Protocol<short> : public four_byte<short>{};
+template <> struct Protocol<long> : public eight_byte<long>{};
+template <> struct Protocol<long long> : public eight_byte<long long>{};
+template <> struct Protocol<float> : public eight_byte<float>{};
+template <> struct Protocol<double> : public eight_byte<double>{};
+template <> struct Protocol<unsigned int> : public four_byte<unsigned int>{};
+template <> struct Protocol<unsigned char> : public four_byte<unsigned char>{};
+template <> struct Protocol<unsigned short> : public four_byte<unsigned short>{};
+template <> struct Protocol<unsigned long> : public eight_byte<unsigned long>{};
+template <> struct Protocol<unsigned long long> : public eight_byte<unsigned long long>{};
+// template <> struct Protocol<unsigned float> : public eight_byte<unsigned float>{};
+// template <> struct Protocol<unsigned double> : public eight_byte<unsigned double>{};
 
 // TASK2: Client-side
 class IntParam : public BaseParams {
